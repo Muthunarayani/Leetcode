b1e57ae9d2fad72f326a7113f1f552e3194e0e36
Muthunarayani <narayanan151956@gmail.com>
2025-01-20 19:37:03 +0530

    initial

--- ArrayString/Gcdstring.java
+++ ArrayString/Gcdstring.java
@@ -1,6 +1,6 @@
 class Gcdstring{
     public static String calculate(String s1, String s2){
-        if(s1+s2 != s2+s1)
+        if(!(s1+s2).equals(s2+s1))
          return "";
         int ans= gcd(s1.length(), s2.length());
         return s1.substring(0, ans);
@@ -11,7 +11,7 @@ class Gcdstring{
         return b==0 ? a : gcd(b,a%b);
     }
     public static void main(String[] args) {
-        String s1 = "ABABAB";  
+        String s1 = "ABAB";  
         String s2 = "AB";  
        System.out.println(calculate(s1,s2));
 }
--- ArrayString/Prime.java
+++ ArrayString/Prime.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class prime{
+class Prime{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
